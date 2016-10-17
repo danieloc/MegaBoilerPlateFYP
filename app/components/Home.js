c29e64ee1b8fe2bcfd1670c5df680b4ca84fdbd3
danieloc <daniel.o.c.03@gmail.com>
2016-10-17 22:58:47 +0100

    More Quick Fixes

--- app/components/Home.js
+++ app/components/Home.js
@@ -12,7 +12,18 @@ class Home extends React.Component {
             <div className="panel">
               <div className="panel-body">
                 <h3>Heading</h3>
-                <p>Donec i elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+                  mollis euismod. Donec sed odio dui.</p>
+                <a href="#" role="button" className="btn btn-default">View details</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="panel">
+              <div className="panel-body">
+                <h3>Heading</h3>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
                   mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
                   mollis euismod. Donec sed odio dui.</p>
                 <a href="#" role="button" className="btn btn-default">View details</a>
