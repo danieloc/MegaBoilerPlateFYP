db22ba5070bc58d43ef43a343f37d54d567bc367
danieloc <daniel.o.c.03@gmail.com>
2016-11-28 18:25:42 +0000

    Add check to to see if goal already exists

--- app/components/addGoals.js
+++ app/components/addGoals.js
@@ -13,6 +13,7 @@ class addGoals extends React.Component {
             email: props.user.email,
             name: props.user.name,
             gender: props.user.gender,
+            goals: props.user.goals,
             goal: '',
             priority: ''
         }
